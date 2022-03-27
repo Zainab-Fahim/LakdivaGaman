53b53960170061124f9b59389dea9b1e878bf0ef
Ranmini Welagedara <ranmini.20210471@iit.ac.lk>
2022-03-27 23:01:13 +0530

    updated changes

---
+++ js/sitemap.js
@@ -0,0 +1,34 @@
+document.getElementById("home").addEventListener("click",function(){
+		document.location.href="../index.html";
+		});
+		document.getElementById("pack").addEventListener("click",function(){
+		document.location.href="../pages/packages.html";
+		});
+		document.getElementById("quiz").addEventListener("click",function(){
+		document.location.href="../pages/quiz.html";
+		});
+		
+		document.getElementById("gallery").addEventListener("click",function(){
+		document.location.href="../pages/gallery.html";
+		});
+		document.getElementById("contact").addEventListener("click",function(){
+		document.location.href="../index.html#contact";
+		});
+		document.getElementById("st1").addEventListener("click",function(){
+		document.location.href="../pages/rimaz.html";
+		});
+		document.getElementById("st2").addEventListener("click",function(){
+		document.location.href="../pages/dilitha.html";
+		});
+		document.getElementById("st3").addEventListener("click",function(){
+		document.location.href="../pages/zainab.html";
+		});
+		document.getElementById("st4").addEventListener("click",function(){
+		document.location.href="../pages/ranmini.html";
+		});
+		document.getElementById("review").addEventListener("click",function(){
+			document.location.href="../index.html#review";
+		});
+		document.getElementById("team").addEventListener("click",function(){
+			document.location.href="../index.html#team";
+		});
